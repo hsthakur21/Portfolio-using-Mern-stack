b37e26eaddb3737ae6d77fe6a113c0a4a31b0933
Himanshu Thakur <himanshussthakur2002@gmail.com>
2024-08-10 00:41:07 -0700

    mongo

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,7 @@ const port = process.env.PORT
 
 // Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/portfolio', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+ 
 })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log('Error connecting to MongoDB:', err));
