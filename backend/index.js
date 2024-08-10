322f154c19c20f14a540382be451d4643887210c
Himanshu Thakur <himanshussthakur2002@gmail.com>
2024-08-10 00:22:39 -0700

    envfile add

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT 
 
 // Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/portfolio', {
